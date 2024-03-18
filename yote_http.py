41f746e14418867fbf318aa84eee79c8b7077497
Kayla Coyote <kayla@kilo.cc>
2024-03-18 00:22:48 -0700

    Make it easier to trigger 418. >:P

--- yote_http.py
+++ yote_http.py
@@ -237,6 +237,10 @@ def GET(req: RequestHeader) -> Response:
 # -------------------------------------
 # Nonsense.
 
+ENABLE_HTCPCP = True
+IMPLY_HTCPCP_FROM_CONTENT_TYPE = True
+IMPLY_HTCPCP_FROM_METHOD_BREW = True
+
 HTCPCP_SCHEMES = set([x.lower() for x in [
    'koffie',      # Afrikaans, Dutch
    'q\xC3\xA6hv\xC3\xA6',              # Azerbaijani
@@ -248,7 +252,7 @@ HTCPCP_SCHEMES = set([x.lower() for x in [
    'caf\xC3\xE8', # Catalan, French, Galician
    '\xE5\x92\x96\xE5\x95\xA1',         # Chinese
    'kava',        # Croatian
-   'k\xC3\xA1va', # Czech
+   'k\xC3\xA1va', # Czech  <- My errata! -Kayla
    'kaffe',       # Danish, Norwegian, Swedish
    'coffee',      # English
    'kafo',        # Esperanto
@@ -262,41 +266,64 @@ HTCPCP_SCHEMES = set([x.lower() for x in [
    '\xD0\xBA\xD0\xBE\xD1\x84\xD0\xB5', # Russian
    '\xE0\xB8\x81\xE0\xB8\xB2\xE0\xB9\x81\xE0\xB8\x9F', # Thai
 ]])
+def is_scheme_htcpcp(scheme: str) -> bool:
+   assert not scheme.endswith(':'), scheme
+   if scheme.lower() not in HTCPCP_SCHEMES:
+      return False
 
-COFFEEPOTS: list[str] = [
-   '/',
-]
-TEAPOTS: list[str] = [
-#   '/pot-0/darjeeling',
-#   '/pot-0/earl-grey',
-#   '/pot-1/peppermint',
-]
+   GERMAN_SCHEME = '%4Baffee'
+   if scheme.lower() == GERMAN_SCHEME.lower() and scheme[0] != GERMAN_SCHEME[0]:
+      # > Note that while URL scheme names are case-independent, capitalization is
+      # > important for German and thus the initial "K" must be encoded.
+      return False
+
+   return True
+
+
+
+HTCPCP_CONTENT_TYPES_COFFEE = set(['message/coffeepot', 'application/coffee-pot-command'])
+HTCPCP_CONTENT_TYPES_TEA = set(['message/teapot'])
+HTCPCP_CONTENT_TYPES = HTCPCP_CONTENT_TYPES_COFFEE | HTCPCP_CONTENT_TYPES_TEA
+def is_request_htcpcp(req: RequestHeader) -> bool:
+   scheme = req.uri.scheme
+   if scheme:
+      return is_scheme_htcpcp(scheme)
+
+   if IMPLY_HTCPCP_FROM_METHOD_BREW:
+      if req.method == 'BREW':
+         return True
+   if IMPLY_HTCPCP_FROM_CONTENT_TYPE:
+      ct = req.headers.get('Content-Type')
+      if ct in HTCPCP_CONTENT_TYPES:
+         return True
+
+   return False
+
+
+CONTENT_TYPE_BY_POT_PATH: dict[str,str] = {
+   #'/': 'message/coffeepot',
+   #'/tea': 'message/teapot',
+   '/kofi': 'message/coffeepot',
+}
 
 def htcpcp_request(req: RequestHeader) -> Optional[Response]:
    # HTCPCP: https://datatracker.ietf.org/doc/html/rfc2324
    # HTCPCP-TEA: https://datatracker.ietf.org/doc/html/rfc7168
 
-   GERMAN_SCHEME = '%4Baffee'
-   scheme = req.uri.scheme
-   if scheme.lower() == GERMAN_SCHEME.lower() and scheme[0] != GERMAN_SCHEME[0]:
-      # > Note that while URL scheme names are case-independent, capitalization is
-      # > important for German and thus the initial "K" must be encoded.
-      return None
-
    if req.method in ['BREW','POST']:
-      ct = req.headers.get('Content-Type')
-      if ct in ['message/coffeepot', 'application/coffee-pot-command']:
-         if req.uri.path in COFFEEPOTS:
-            return Response(402, f'https://ko-fi.com/kaylayote', {})
-         if TEAPOTS:
-            return Response(418, 'Tea only!', {})
-         return Response(404, f'No coffeepot at requested URI.', {})
+      header_ct = req.headers.get('Content-Type')
+      if header_ct and header_ct not in HTCPCP_CONTENT_TYPES:
+         return Response(501, f'Unrecognized Content-Type "{header_ct}" not in {HTCPCP_CONTENT_TYPES}.', {})
 
-      if ct == 'message/teapot':
-         if req.uri.path in TEAPOTS:
-            return Response(402, f'https://ko-fi.com/kaylayote', {})
-         return Response(404, f'No teapot at requested URI.', {})
-      return Response(501, f'Unrecognized Content-Type: {ct}', {})
+      pot_ct = CONTENT_TYPE_BY_POT_PATH.get(req.uri.path, None)
+      if header_ct != pot_ct:
+         if header_ct != 'message/teapot':
+            return Response(418, "I'm a teapot", {})
+         else:
+            return Response(418, "I'm NOT a teapot", {})
+
+      return Response(402, f'https://ko-fi.com/kaylayote', {})
+
 
    if req.method == 'GET':
       return Response(204, f'No caffeine may be retrieved electronically.', {})
@@ -310,7 +337,7 @@ def htcpcp_request(req: RequestHeader) -> Optional[Response]:
 # -------------------------------------
 
 def response_from_request(req: RequestHeader) -> Response:
-   if req.uri.scheme.lower() in HTCPCP_SCHEMES:
+   if ENABLE_HTCPCP and is_request_htcpcp(req):
       if res := htcpcp_request(req):
          return res
 
